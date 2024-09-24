233140befff87d230f94a182e5bee2a1bf77324d
miku-wwl <569144003@qq.com>
2024-09-24 23:34:50 +0800

    feat:实现最简单的身份认证

---
+++ src/main/java/com/weilai/hsec/HsecApplication.java
@@ -0,0 +1,13 @@
+package com.weilai.hsec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HsecApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HsecApplication.class, args);
+    }
+
+}
