aadcc4866026826fe5326548dcfe3cf36e5a3db7
miku-wwl <569144003@qq.com>
2024-09-26 00:59:52 +0800

    feat:基于数据库的用户认证

--- src/main/java/com/weilai/hsec/config/WebSecurityConfig.java
+++ src/main/java/com/weilai/hsec/config/WebSecurityConfig.java
@@ -2,24 +2,38 @@ package com.weilai.hsec.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 public class WebSecurityConfig {
 
+//    @Bean
+//    public UserDetailsService userDetailsService() {
+//        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+//        manager.createUser( //此行设置断点可以查看创建的user对象
+//                User
+//                        .withDefaultPasswordEncoder()
+//                        .username("huan") //自定义用户名
+//                        .password("password") //自定义密码
+//                        .roles("USER") //自定义角色
+//                        .build()
+//        );
+//        return manager;
+//    }
+
+
     @Bean
-    public UserDetailsService userDetailsService() {
-        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
-        manager.createUser( //此行设置断点可以查看创建的user对象
-                User
-                        .withDefaultPasswordEncoder()
-                        .username("huan") //自定义用户名
-                        .password("password") //自定义密码
-                        .roles("USER") //自定义角色
-                        .build()
-        );
-        return manager;
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        //authorizeRequests()：开启授权保护
+        //anyRequest()：对所有请求开启授权保护
+        //authenticated()：已认证请求会自动被授权
+        http.authorizeHttpRequests(authorize -> authorize.anyRequest().authenticated())
+                .formLogin(withDefaults());//表单授权方式
+//                .httpBasic(withDefaults());//基本授权方式
+
+        return http.build();
     }
 }
