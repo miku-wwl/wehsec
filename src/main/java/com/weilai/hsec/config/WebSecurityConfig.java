ef09ef19f48b8949152d35a3cb5321e457166bb0
miku-wwl <569144003@qq.com>
2024-09-26 23:28:21 +0800

    feat:基于request的授权 用户-权限-资源

--- src/main/java/com/weilai/hsec/config/WebSecurityConfig.java
+++ src/main/java/com/weilai/hsec/config/WebSecurityConfig.java
@@ -1,11 +1,14 @@
 package com.weilai.hsec.config;
 
+import com.alibaba.fastjson2.JSON;
 import com.weilai.hsec.config.securityHandler.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+
+import java.util.HashMap;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -32,16 +35,28 @@ public class WebSecurityConfig {
         //authorizeRequests()：开启授权保护
         //anyRequest()：对所有请求开启授权保护
         //authenticated()：已认证请求会自动被授权
-        http.authorizeHttpRequests(authorize -> authorize.anyRequest().authenticated())
-                .formLogin(form -> {
-                    form
-                            .loginPage("/login").permitAll() //登录页面无需授权即可访问
-                            .usernameParameter("username") //自定义表单用户名参数，默认是username
-                            .passwordParameter("password") //自定义表单密码参数，默认是password
-                            .failureUrl("/login?error"); //登录失败的返回地址
-                    form.successHandler(new MyAuthenticationSuccessHandler()); //认证成功时的处理
-                    form.failureHandler(new MyAuthenticationFailureHandler()); //认证失败时的处理
-                }); //使用表单授权方式
+        //开启授权保护
+        http.authorizeHttpRequests(
+                authorize -> authorize
+                        //具有USER_LIST权限的用户可以访问/user/list
+                        .requestMatchers("/user/list").hasAuthority("USER_LIST")
+                        //具有USER_ADD权限的用户可以访问/user/add
+                        .requestMatchers("/user/add").hasAuthority("USER_ADD")
+                        //对所有请求开启授权保护
+                        .anyRequest()
+                        //已认证的请求会被自动授权
+                        .authenticated()
+        );
+
+        http.formLogin(form -> {
+            form
+                    .loginPage("/login").permitAll() //登录页面无需授权即可访问
+                    .usernameParameter("username") //自定义表单用户名参数，默认是username
+                    .passwordParameter("password") //自定义表单密码参数，默认是password
+                    .failureUrl("/login?error"); //登录失败的返回地址
+            form.successHandler(new MyAuthenticationSuccessHandler()); //认证成功时的处理
+            form.failureHandler(new MyAuthenticationFailureHandler()); //认证失败时的处理
+        }); //使用表单授权方式
         http.csrf(AbstractHttpConfigurer::disable);
         http.logout(logout -> {
             logout.logoutSuccessHandler(new MyLogoutSuccessHandler()); //注销成功时的处理
@@ -49,6 +64,19 @@ public class WebSecurityConfig {
         //错误处理
         http.exceptionHandling(exception -> {
             exception.authenticationEntryPoint(new MyAuthenticationEntryPoint());//请求未认证的接口
+            exception.accessDeniedHandler((request, response, e) -> { //请求未授权的接口
+                //创建结果对象
+                HashMap result = new HashMap();
+                result.put("code", -1);
+                result.put("message", "没有权限");
+
+                //转换成json字符串
+                String json = JSON.toJSONString(result);
+
+                //返回响应
+                response.setContentType("application/json;charset=UTF-8");
+                response.getWriter().println(json);
+            });
         });
         //跨域
         http.cors(withDefaults());
